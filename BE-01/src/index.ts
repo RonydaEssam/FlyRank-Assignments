9e99a7e3447b8e2fbc75c2baa189f79d52d02131
Ronyda Essam <ronyda988@gmail.com>
2026-08-13 15:51:21 +0300

    Stage 1: root and health endpoints

--- BE-01/src/index.ts
+++ BE-01/src/index.ts
@@ -4,7 +4,15 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from the Task API server!');
+    res.status(200).json({
+        name: 'Task Api',
+        version: '1.0',
+        endpoints: ['/tasks']
+    });
+})
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok' })
 })
 
 app.listen(PORT, () => {
